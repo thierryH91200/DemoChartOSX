476660b69afe67c76c57f1e62a4a03f35d7a71a0
thierryH91200 <thierry.hentic@gmail.com>
2017-08-31 20:28:29 +0200

    init

---
+++ DemoChartOSX/MapForItinerary.swift
@@ -0,0 +1,17 @@
+//
+//  MapForItinerary.swift
+//  DemoChartOSX
+//
+//  Created by thierryH24100 on 31/08/2017.
+//  Copyright © 2017 thierryH24100. All rights reserved.
+//
+
+import Cocoa
+
+class MapForItinerary: NSObject {
+
+    
+    func sayHello() {
+        print("Hello from MapForItinerary")
+    }
+}
